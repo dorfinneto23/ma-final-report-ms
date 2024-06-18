8d1db606f0028d5e177bda3b70824d76dca89c96
tech dor <tech.aharoni@gmail.com>
2024-06-18 17:38:43 +0300

    adding translate_text

--- function_app.py
+++ function_app.py
@@ -10,12 +10,17 @@ from openai import AzureOpenAI #for using openai services
 from azure.data.tables import TableServiceClient, TableClient, UpdateMode # in order to use azure storage table  
 from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError # in order to use azure storage table  exceptions 
 import csv #helping convert json to csv
+import requests #in order to use translation function 
+import uuid  #in order to use translation function 
 
 # Azure Blob Storage connection string
 connection_string_blob = os.environ.get('BlobStorageConnString')
 
 #Azure service bus connection string 
 connection_string_servicebus = os.environ.get('servicebusConnectionString')
+
+#translate key
+translate_key = os.environ.get('translate_key')
 
 # Define connection details
 server = 'medicalanalysis-sqlserver.database.windows.net'
@@ -25,9 +30,40 @@ password = os.environ.get('sql_password')
 driver= '{ODBC Driver 18 for SQL Server}'
 
 
+#Translate conent language 
+def translate_text(text, to_language='he'):
+    key = translate_key
+    endpoint = "https://api.cognitive.microsofttranslator.com/"
+    location = "eastus"
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': 'en',
+        'to': [to_language]
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text': text
+    }]
+
+    response = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response.raise_for_status()
+
+    translations = response.json()
+    translated_text = translations[0]['translations'][0]['text']
+    return translated_text
 
 #save ContentByClinicAreas content 
-def save_union_clinic_areas(content,caseid,filename):
+def save_final_files(content,caseid,filename):
     try:
         logging.info(f"save_ContentByClinicAreas start, content: {content},caseid: {caseid},filename: {filename}")
         container_name = "medicalanalysis"
@@ -76,15 +112,20 @@ def union_clinic_areas(table_name, caseid):
 
     # union assistantResponsefiltered into one file for each entity
     combined_content = ""
-    file_name = f"final-{caseid}.txt"
+    union_file_name = f"final-{caseid}.txt"
     for entity in entities:
         clinic_area = entity['RowKey']
         content_path = entity['assistantResponsefiltered']
         filecontent = get_contentcsv(content_path)
         if filecontent!="no disabilities found.":
             combined_content += "# " + clinic_area + "\n" + filecontent + "\n"
-    save_union_clinic_areas(combined_content,caseid,file_name)
+    #save union content of all clinic areas         
+    save_final_files(combined_content,caseid,union_file_name)
+    text_heb = translate_text(combined_content)
+    heb_file_name = f"final-{caseid}-heb.txt"
+    save_final_files(text_heb,caseid,heb_file_name)
     logging.info(f"union_clinic_areas: combined_content done")
+    
 
 
 app = func.FunctionApp()
@@ -96,4 +137,5 @@ def finalReportMs(azservicebus: func.ServiceBusMessage):
     logging.info(f"Received messageesds: {message_data}")
     message_data_dict = json.loads(message_data)
     caseid = message_data_dict['caseid']
-    union_clinic_areas("contentByClinicAreas",caseid)
+    union_clinic_areas_path = union_clinic_areas("contentByClinicAreas",caseid)
+    logging.info(f"union_clinic_areas path: {union_clinic_areas_path}")
