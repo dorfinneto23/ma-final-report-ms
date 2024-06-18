edd30bffcbd4a3057eaa78d4ac515cb78a33afef
tech dor <tech.aharoni@gmail.com>
2024-06-18 12:33:22 +0300

    save union clinic areas

--- function_app.py
+++ function_app.py
@@ -25,10 +25,73 @@ password = os.environ.get('sql_password')
 driver= '{ODBC Driver 18 for SQL Server}'
 
 
+
+#save ContentByClinicAreas content 
+def save_union_clinic_areas(content,caseid,filename):
+    try:
+        logging.info(f"save_ContentByClinicAreas start, content: {content},caseid: {caseid},filename: {filename}")
+        container_name = "medicalanalysis"
+        main_folder_name = "cases"
+        folder_name="case-"+caseid
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string_blob)
+        container_client = blob_service_client.get_container_client(container_name)
+        basicPath = f"{main_folder_name}/{folder_name}"
+        destinationPath = f"{basicPath}/final/{filename}"
+        # Upload the blob and overwrite if it already exists
+        blob_client = container_client.upload_blob(name=destinationPath, data=content, overwrite=True)
+        logging.info(f"the ContentByClinicAreas content file url is: {blob_client.url}")
+        return destinationPath
+    
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+# get content csv path from azure table storage 
+def get_contentcsv(path):
+    try:
+        logging.info(f"get_contentcsv function strating, path value: {path}")
+        container_name = "medicalanalysis"
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string_blob)
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_client = container_client.get_blob_client(path)
+        download_stream = blob_client.download_blob()
+        filecontent  = download_stream.read().decode('utf-8')
+        logging.info(f"get_contentcsv: data from the txt file is {filecontent}")
+        return filecontent
+    except Exception as e:
+        logging.error(f"get_contentcsv: Error update case: {str(e)}")
+        return None    
+    
+
+#this function union all clinic areas content into one file 
+def union_clinic_areas(table_name, caseid):
+    # Create a TableServiceClient object using the connection string
+    service_client = TableServiceClient.from_connection_string(conn_str=connection_string_blob)
+    
+    # Get the table client
+    table_client = service_client.get_table_client(table_name=table_name)
+    
+
+    # Query the table for entities with the given PartitionKey
+    entities = table_client.query_entities(f"PartitionKey eq '{caseid}'")
+
+    # Print the RowKey for each entity
+    combined_content = ""
+    file_name = f"final-{caseid}.txt"
+    for entity in entities:
+        content_path = entity['assistantResponsefiltered']
+        filecontent = get_contentcsv(content_path)
+        combined_content += filecontent + "\n"
+    save_union_clinic_areas(combined_content,caseid,file_name)
+    logging.info(f"union_clinic_areas: combined_content done")
+
+
 app = func.FunctionApp()
 
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="final-report-process",
                                connection="medicalanalysis_SERVICEBUS") 
 def finalReportMs(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue trigger processed a messagee: %s',
-                azservicebus.get_body().decode('utf-8'))
+    message_data = azservicebus.get_body().decode('utf-8')
+    logging.info(f"Received messageesds: {message_data}")
+    message_data_dict = json.loads(message_data)
+    caseid = message_data_dict['caseid']
+    union_clinic_areas("contentByClinicAreas",caseid)
